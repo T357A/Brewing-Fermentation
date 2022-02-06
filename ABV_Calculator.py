815021ea11d09823d8838437bec263a73b8b36a5
T357A <T357A@users.noreply.github.com>
2022-02-06 17:44:16 -0500

    Added (less accurate) calc found online that is used by websites

--- ABV_Calculator.py
+++ ABV_Calculator.py
@@ -6,6 +6,15 @@ final_gravity = float(input("What is your FG (Final Gravity I.E. 1.000) reading?
 alc_by_vol = (
     76.08 * (original_gravity - final_gravity) / (1.775 - original_gravity)
 ) * (final_gravity / 0.794)
+# Online simple calculator- less accurate:
+abv_ol = original_gravity - final_gravity * 131.25
 # Using string formating to limit to 2 decimal places
 abv = "{:.2f}".format(alc_by_vol)
-print("Your alcohol by volume or ABV is: " + abv + "%")
+abv_online = "{:.2f}".format(abv_ol)
+print("Your ABV is: " + abv + "%")
+print(
+    "An online calculator may have this (less accurate) result:"
+    + abv_online
+    + "%"
+    + "ABV"
+)
